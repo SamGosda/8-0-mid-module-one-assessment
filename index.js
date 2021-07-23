9efe7e1926372d12417c7f64c6eb383ee778da5c
Samantha Gosda <gosdasam@gmail.com>
2021-07-23 10:11:07 -0400

    finished getTotalCount function

--- index.js
+++ index.js
@@ -26,7 +26,15 @@ const animals = [
  * getTotalCount(animals); //> 28
  * getTotalCount([]); //> 0 // returns 0 if the input array is empty
  */
-function getTotalCount(animals) {}
+function getTotalCount(animals) {
+  let totalAnimalCount = 0;
+  for(let i=0; i<animals.length; i++){
+    totalAnimalCount += animals[i].count;
+  }
+  return totalAnimalCount;
+}
+
+console.log(getTotalCount(animals));
 
 /**
  * FUNCTION DESCRIPTION
